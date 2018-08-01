2c9c7127dafa41cb64e80bb8da11cee6861faf8f
HUA Di <huadi223@gmail.com>
2018-08-01 17:15:04 +0800

    change sync load to async preload

--- src/main/java/huadi/PkSequence.java
+++ src/main/java/huadi/PkSequence.java
@@ -5,6 +5,9 @@ import org.slf4j.LoggerFactory;
 
 import java.sql.*;
 import java.util.Properties;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicLong;
 
 import static huadi.Const.*;
@@ -30,6 +33,7 @@ public class PkSequence {
         } catch (ClassNotFoundException e) {
             throw new Error("No JDBC driver.", e);
         }
+        preload();
     }
 
     private String sequenceTableName = DEFAULT_SEQUENCE_TABLE_NAME;
@@ -41,6 +45,9 @@ public class PkSequence {
 
     // 初始化一个不能用的pool, 第一次使用会触发更新. 省得每次在get方法中做null判断.
     private SeqPool seqPool = new SeqPool(0, -1);
+    private SeqPool preloadSeqPool;
+
+    private AtomicBoolean onPreload = new AtomicBoolean();
 
 
     public Long get() {
@@ -48,7 +55,13 @@ public class PkSequence {
         if ((primaryKey = seqPool.next()) == SeqPool.INVALID_VALUE) {
             synchronized (this) {
                 while ((primaryKey = seqPool.next()) == SeqPool.INVALID_VALUE) {
-                    seqPool = load();
+                    if (preloadSeqPool != null) {
+                        seqPool = preloadSeqPool;
+                        preloadSeqPool = null;
+                    } else {
+                        logger.warn("[PkSeq] Switch pool fail. " +
+                                "It may be caused by heavy request, try to increase value of 'step' column.");
+                    }
                 }
             }
         }
@@ -56,43 +69,63 @@ public class PkSequence {
     }
 
 
-    private SeqPool load() {
-        // 高并发情况下, 优先考虑增大step减少load次数, 所以这里并不需要强制使用连接池.
-        try (Connection c = DriverManager.getConnection(databaseConfig.getProperty(PROP_CONN_URL),
-                databaseConfig.getProperty(PROP_CONN_USERNAME), databaseConfig.getProperty(PROP_CONN_PASSWORD))) {
-            while (true) {
-                Long oldValue, stepValue, newValue;
-                try (Statement s = c.createStatement()) {
-                    String sql = "SELECT * FROM " + sequenceTableName + " WHERE " + KEY_COLUMN + "='" + name + "'";
-                    try (ResultSet r = s.executeQuery(sql)) {
-                        r.next();
-                        stepValue = r.getLong(STEP_COLUMN);
-                        if (stepValue <= 0) { // step不能为0, 会导致在get无限循环.
-                            throw new RuntimeException("Step cannot be 0.");
-                        }
-                        oldValue = r.getLong(VALUE_COLUMN);
-                        newValue = oldValue + stepValue; // 这里有可能溢出
-                    }
-                    sql = "UPDATE " + sequenceTableName + " SET " + VALUE_COLUMN + "=" + newValue +
-                            " WHERE " + KEY_COLUMN + "='" + name + "' AND " + VALUE_COLUMN + "=" + oldValue;
-                    if (s.executeUpdate(sql) != 0) {
-                        logger.info("[PkSeq] SeqPool loaded. Min={}, max={}.", oldValue, newValue);
-                        // 这里使用oldValue作为seqPool, 这样所有小于db中VALUE_COLUMN域的seq都可以认为是使用过的,
-                        // 在应用运行过程中可以随时修改step大小.
-                        return new SeqPool(oldValue + 1, newValue);
-                    }
-                }
-                logger.warn("[PkSeq] SeqPool load conflict, retry. KeyName: \"{}\", old: {}, new: {}, step: {}.",
-                        name, oldValue, newValue, stepValue);
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException("Exception on loading sequence.", e);
+    private ExecutorService preloadThreadPool = Executors
+            .newSingleThreadExecutor(r -> new Thread(r, "PkSequencePreloadThread-" + name));
+
+    private void preload() {
+        if (preloadSeqPool != null || onPreload.get() || !onPreload.compareAndSet(false, true)) {
+            return;
         }
+
+        preloadThreadPool.submit(() -> {
+            try (Connection c = getConn()) {
+                while (true) {
+                    Long oldValue, stepValue, newValue;
+                    try (Statement s = c.createStatement()) {
+                        String sql = "SELECT * FROM " + sequenceTableName + " WHERE " + KEY_COLUMN + "='" + name + "'";
+                        try (ResultSet r = s.executeQuery(sql)) {
+                            r.next();
+                            stepValue = r.getLong(STEP_COLUMN);
+                            if (stepValue <= 0) { // step不能为0, 会导致在get无限循环.
+                                throw new RuntimeException("Step cannot be 0.");
+                            }
+                            oldValue = r.getLong(VALUE_COLUMN);
+                            newValue = oldValue + stepValue; // 这里有可能溢出
+                        }
+                        sql = "UPDATE " + sequenceTableName + " SET " + VALUE_COLUMN + "=" + newValue +
+                                " WHERE " + KEY_COLUMN + "='" + name + "' AND " + VALUE_COLUMN + "=" + oldValue;
+                        if (s.executeUpdate(sql) != 0) {
+                            logger.info("[PkSeq] SeqPool loaded. Min={}, max={}.", oldValue, newValue);
+                            // 这里使用oldValue作为seqPool, 这样所有小于db中VALUE_COLUMN域的seq都可以认为是使用过的,
+                            // 在应用运行过程中可以随时修改step大小.
+                            preloadSeqPool = new SeqPool(oldValue + 1, newValue);
+                        }
+                    }
+                    logger.warn("[PkSeq] SeqPool load conflict, retry. KeyName: \"{}\", old: {}, new: {}, step: {}.",
+                            name, oldValue, newValue, stepValue);
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException("Exception on loading sequence.", e);
+            } finally {
+                onPreload.set(false);
+            }
+        });
+    }
+
+    private Connection getConn() throws SQLException {
+        return DriverManager.getConnection(databaseConfig.getProperty(PROP_CONN_URL),
+                databaseConfig.getProperty(PROP_CONN_USERNAME), databaseConfig.getProperty(PROP_CONN_PASSWORD));
     }
 
     // 对于get方法来讲, pk和max必须保证原子更新, 所以这里用一个class封装.
-    private static class SeqPool {
+    private class SeqPool {
+        // 返回此值时，表示此pool的seq已经消耗完，需要执行switch
         static final long INVALID_VALUE = Long.MIN_VALUE;
+        // preload的seq值，达到此seq时，执行preload
+        long preloadSeq;
+        // 用来评估preloadSeq的时间戳
+        long createTimestamp = System.currentTimeMillis();
+
 
         AtomicLong seq;
         long max;
@@ -100,10 +133,17 @@ public class PkSequence {
         SeqPool(long seq, long max) {
             this.seq = new AtomicLong(seq);
             this.max = max;
+
+            preloadSeq = seq + (long) ((max - seq) * 0.8);
         }
 
         long next() {
             long v = seq.getAndIncrement();
+
+            if (v > preloadSeq) {
+                preload();
+            }
+
             return v > max ? INVALID_VALUE : v;
         }
     }
